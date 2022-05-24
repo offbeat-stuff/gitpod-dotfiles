2ade9b361ddcb9a72bb37cb5da8937643e444bc7
offbeat-stuff <34603072+offbeat-stuff@users.noreply.github.com>
2022-05-24 12:30:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo pacman -Sy --no-confirm fish neovim
 cat fish_shell.sh >> ~/.bashrc
 curl -s "https://get.sdkman.io" | bash && source ~/.sdkman/bin/sdkman-init.sh && sdk update && sdk install java 17.0.2-zulu && sdk default java 17.0.2-zulu && sdk install scala
 curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs
