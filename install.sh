8263e043e7ec64bd899c80ea8fd21de9adbd32a9
ZenXArch mobile <offbeat-stuff@github.com>
2022-05-24 19:21:30 +0530

    no install cs

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cat fish_shell.sh >> ~/.bashrc
 curl -s "https://get.sdkman.io" | bash && source ~/.sdkman/bin/sdkman-init.sh && sdk update && sdk install java 17.0.2-zulu && sdk default java 17.0.2-zulu && sdk install scala
-curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs
-chmod +x cs
-./cs setup -y
+cp ./install-cs.sh ~/
 ./install.fish
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && cp -R ./custom ~/.config/nvim/lua
