983c42d3aea4473f92bddd4f6aa9c74bd2c5cb5d
offbeat-stuff <34603072+offbeat-stuff@users.noreply.github.com>
2022-05-24 09:40:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-apt-get install -yq --no-install-recommends fish git neovim
+apt-get install -y fish git neovim
 cat fish_shell.sh >> ~/.bashrc
 ~/.sdkman/bin/sdkman-init.sh && sdk update && sdk install java 17.0.2-zulu && sdk default java 17.0.2-zulu && sdk install scala
 curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs
 chmod +x cs
-./cs setup
+./cs setup -y
 ./install.fish
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && cp -R ./custom ~/.config/nvim/lua && nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && cp -R ./custom ~/.config/nvim/lua
