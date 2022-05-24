ccc20fb936526ae3e4b7f16a0de1d77d1c83e706
ZenXArch mobile <offbeat-stuff@github.com>
2022-05-24 12:19:30 +0530

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-apt-get install -yq --no-install-recommends fish git neovim
+sudo pacman -Sy --no-confirm fish neovim
 cat fish_shell.sh >> ~/.bashrc
-~/.sdkman/bin/sdkman-init.sh && sdk update && sdk install java 17.0.2-zulu && sdk default java 17.0.2-zulu && sdk install scala
+curl -s "https://get.sdkman.io" | bash && source ~/.sdkman/bin/sdkman-init.sh && sdk update && sdk install java 17.0.2-zulu && sdk default java 17.0.2-zulu && sdk install scala
 curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs
 chmod +x cs
-./cs setup
 ./install.fish
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && cp -R ./custom ~/.config/nvim/lua && nvim
